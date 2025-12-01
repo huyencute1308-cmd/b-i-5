e8eb59820b9093985f10cb0862f04cb12bea9b13
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 20:54:19 +0700

    Update 5.4.4.py

--- 5.4.4.py
+++ 5.4.4.py
@@ -1,11 +1,12 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-==== RESTART: C:/Users/HH/AppData/Local/Programs/Python/Python313/5.4.11.py ====
-[('Pit', 5, 40.11) ('Paul', 5, 42.1 ) ('James', 5, 48.5 )
- ('Nail', 6, 52.5 )]
->>> 
-==== RESTART: C:/Users/HH/AppData/Local/Programs/Python/Python313/5.4.12.py ====
-Các chỉ số sắp xếp: [4 0 5 3 6 1 2]
-Student IDs sau khi sắp xếp: [1682 1023 5241 1671 4532 5202 6230]
-Student heights sau khi sắp xếp: [38. 40. 40. 41. 42. 42. 45.]
+import numpy as np
+
+# Tạo mảng từ 12 đến 38
+arr = np.arange(12, 39)
+print("Mảng ban đầu:")
+print(arr)
+
+# Đảo ngược mảng
+reversed_arr = arr[::-1]
+print("Mảng sau khi đảo ngược:")
+print(reversed_arr)
+
