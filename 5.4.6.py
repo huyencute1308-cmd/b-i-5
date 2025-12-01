240449ed7c86b4506f7c2f7877e169ca0a0fa223
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 21:22:16 +0700

    Update 5.4.6.py

--- 5.4.6.py
+++ 5.4.6.py
@@ -1,6 +1,12 @@
+numbers = list(map(int, input("Nhập các số cách nhau bởi dấu cách: ").split()))
+max_value = max(numbers)
+min_value = min(numbers)
 
-def sort_list(lst):
-    return sorted(lst)
+max_index = numbers.index(max_value)
+min_index = numbers.index(min_value)
 
-def find_max(lst):
-    return max(lst)
+print("Giá trị lớn nhất:", max_value, "ở vị trí:", max_index)
+print("Giá trị nhỏ nhất:", min_value, "ở vị trí:", min_index)
+
+
+
