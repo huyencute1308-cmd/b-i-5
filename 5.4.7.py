8fc2ccddb72b78f5a930900be51218093658cea3
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 21:43:16 +0700

    Update 5.4.7.py

--- 5.4.7.py
+++ 5.4.7.py
@@ -1,11 +1,18 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-==== RESTART: C:/Users/HH/AppData/Local/Programs/Python/Python313/5.4.11.py ====
-[('Pit', 5, 40.11) ('Paul', 5, 42.1 ) ('James', 5, 48.5 )
- ('Nail', 6, 52.5 )]
->>> 
-==== RESTART: C:/Users/HH/AppData/Local/Programs/Python/Python313/5.4.12.py ====
-Các chỉ số sắp xếp: [4 0 5 3 6 1 2]
-Student IDs sau khi sắp xếp: [1682 1023 5241 1671 4532 5202 6230]
-Student heights sau khi sắp xếp: [38. 40. 40. 41. 42. 42. 45.]
+import numpy as np
+data_type = [('name', 'S15'), ('class', int), ('height', float)]
+
+# Dữ liệu sinh viên
+students_details = [
+    ('James', 5, 48.5),
+    ('Nail', 6, 52.5),
+    ('Paul', 5, 42.10),
+    ('Pit', 5, 40.11)
+]
+students = np.array(students_details, dtype=data_type)
+
+print("Original array:")
+print(students)
+
+print("Sort by height:")
+print(np.sort(students, order='height'))
+
