9664a1e9f6497700533faa77872b282716e510f9
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 20:48:16 +0700

    Update 5.4.2.py

--- 5.4.2.py
+++ 5.4.2.py
@@ -1,11 +1,21 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-==== RESTART: C:/Users/HH/AppData/Local/Programs/Python/Python313/5.4.11.py ====
-[('Pit', 5, 40.11) ('Paul', 5, 42.1 ) ('James', 5, 48.5 )
- ('Nail', 6, 52.5 )]
->>> 
-==== RESTART: C:/Users/HH/AppData/Local/Programs/Python/Python313/5.4.12.py ====
-Các chỉ số sắp xếp: [4 0 5 3 6 1 2]
-Student IDs sau khi sắp xếp: [1682 1023 5241 1671 4532 5202 6230]
-Student heights sau khi sắp xếp: [38. 40. 40. 41. 42. 42. 45.]
+import datetime as dt
+
+# Chuỗi thời gian cần chuyển đổi
+format = '%Y-%m-%dT%H:%M:%S'
+t1 = dt.datetime.strptime('2008-10-12T14:45:52', format)
+
+# Lấy các thành phần ngày – tháng – giờ – phút – giây
+print('Day ' + str(t1.day))
+print('Month ' + str(t1.month))
+print('Minute ' + str(t1.minute))
+print('Second ' + str(t1.second))
+
+# Lấy thời gian hiện tại của hệ thống
+t2 = dt.datetime.now()
+
+# Tính khoảng cách thời gian (kiểu timedelta)
+diff = t2 - t1
+
+print('How many days difference? ' + str(diff.days))
+
+
