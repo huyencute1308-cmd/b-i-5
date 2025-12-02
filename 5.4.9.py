053272efb0546b91ea3f45d22f9dbebf7360e911
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-02 20:01:39 +0700

    Update 5.4.9.py

--- 5.4.9.py
+++ 5.4.9.py
@@ -1,12 +1,23 @@
 def binary_search(lst, value):
-    lower = 0
-    upper = len(lst) - 1
-    while lower <= upper:
-        mid = (lower + upper) // 2
-        if lst[mid] < value:
-            lower = mid + 1
-        elif lst[mid] > value:
-            upper = mid - 1
-        else:
+    lower_bound = 0
+    upper_bound = len(lst) - 1
+
+    while lower_bound <= upper_bound:
+        mid_point = (lower_bound + upper_bound) // 2
+
+        if lst[mid_point] == value:
             return True
+        elif lst[mid_point] < value:
+            lower_bound = mid_point + 1
+        else:
+            upper_bound = mid_point - 1
+
     return False
+
+
+lst = list(map(int, input("Nhập danh sách (cách nhau bởi dấu cách): ").split()))
+value = int(input("Nhập giá trị cần tìm: "))
+
+found = binary_search(lst, value)
+print("Kết quả tìm kiếm:", found)
+12345678
